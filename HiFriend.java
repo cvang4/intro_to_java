a940444f664c1aa6bcd189fd248ab1d144c39ae3
Choua Vang <c_vang4@outlook.com>
2022-05-02 10:22:41 -0400

    first commit

---
+++ HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+   public static void main(String[] args) {
+       String T = "Hello " + "there " + "friend!";
+       System.out.println(T);
+   }
+}
---
+++ Vacation.java
@@ -0,0 +1,8 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I'd like to visit");
+        System.out.println("South");
+        System.out.println("Korea some day.");
+    }
+
+}
